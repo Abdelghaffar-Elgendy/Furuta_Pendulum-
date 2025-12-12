dd027e4c8664386e1876b39d5dee5d649e58ca74
legion <legion@LAPTOP-2E3QN9H6>
2025-12-12 23:27:41 +0200

    Final Design

---
+++ Simulink_Bodies/Trial_4/Furuta_Project_Assembelly_DataFile1.m
@@ -0,0 +1,203 @@
+% Simscape(TM) Multibody(TM) version: 24.1
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(13).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(13).angle = 0.0;
+smiData.RigidTransform(13).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(13).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0 54.499999999999993 0];  % mm
+smiData.RigidTransform(1).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(1).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(1).ID = "B[encoder.stp-1:-:coupler-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [3.730349362740526e-14 -8.8817841970012523e-15 10.999999999999869];  % mm
+smiData.RigidTransform(2).angle = 3.1415926535897927;  % rad
+smiData.RigidTransform(2).axis = [1 -2.035664850199807e-32 -7.5834930877309855e-17];
+smiData.RigidTransform(2).ID = "F[encoder.stp-1:-:coupler-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [0 0 12.000000000000004];  % mm
+smiData.RigidTransform(3).angle = 0;  % rad
+smiData.RigidTransform(3).axis = [0 0 0];
+smiData.RigidTransform(3).ID = "B[coupler-1:-:2352959-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [1.5626276051213721e-15 1.983820051537665e-15 1.9937889468145515e-14];  % mm
+smiData.RigidTransform(4).angle = 2.0943951023931957;  % rad
+smiData.RigidTransform(4).axis = [-0.57735026918962584 -0.57735026918962562 -0.57735026918962584];
+smiData.RigidTransform(4).ID = "F[coupler-1:-:2352959-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [0 0 0];  % mm
+smiData.RigidTransform(5).angle = 0;  % rad
+smiData.RigidTransform(5).axis = [0 0 0];
+smiData.RigidTransform(5).ID = "B[Bearing Holder-1:-:Encoder Holder-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [4.4408920985006262e-15 -10.00000000000003 -53];  % mm
+smiData.RigidTransform(6).angle = 0;  % rad
+smiData.RigidTransform(6).axis = [0 0 0];
+smiData.RigidTransform(6).ID = "F[Bearing Holder-1:-:Encoder Holder-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [0 5.0000000000000027 0];  % mm
+smiData.RigidTransform(7).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(7).axis = [0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(7).ID = "B[Encoder Holder-1:-:encoder.stp-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-7.460698725481052e-14 39.000000000000085 1.9539925233402755e-14];  % mm
+smiData.RigidTransform(8).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(8).axis = [0 0.70710678118654757 -0.70710678118654757];
+smiData.RigidTransform(8).ID = "F[Encoder Holder-1:-:encoder.stp-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [0 0 150];  % mm
+smiData.RigidTransform(9).angle = 0;  % rad
+smiData.RigidTransform(9).axis = [0 0 0];
+smiData.RigidTransform(9).ID = "B[Box-1:-:Bearing Holder-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [0 0 -2.5];  % mm
+smiData.RigidTransform(10).angle = 0;  % rad
+smiData.RigidTransform(10).axis = [0 0 0];
+smiData.RigidTransform(10).ID = "F[Box-1:-:Bearing Holder-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [0 0 -2];  % mm
+smiData.RigidTransform(11).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(11).axis = [1 0 0];
+smiData.RigidTransform(11).ID = "B[Box-1:-:]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [0 0 1.7763568394002505e-15];  % mm
+smiData.RigidTransform(12).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(12).axis = [-1 0 0];
+smiData.RigidTransform(12).ID = "F[Box-1:-:]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [0 0 2.0000000000000018];  % mm
+smiData.RigidTransform(13).angle = 0;  % rad
+smiData.RigidTransform(13).axis = [0 0 0];
+smiData.RigidTransform(13).ID = "RootGround[Box-1]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(6).mass = 0.0;
+smiData.Solid(6).CoM = [0.0 0.0 0.0];
+smiData.Solid(6).MoI = [0.0 0.0 0.0];
+smiData.Solid(6).PoI = [0.0 0.0 0.0];
+smiData.Solid(6).color = [0.0 0.0 0.0];
+smiData.Solid(6).opacity = 0.0;
+smiData.Solid(6).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.040577949728370609;  % kg
+smiData.Solid(1).CoM = [0.13770452748886594 17.942248535924008 0.00093629125271174997];  % mm
+smiData.Solid(1).MoI = [8.1351677651333372 7.1412007334615808 8.2766069090914716];  % kg*mm^2
+smiData.Solid(1).PoI = [-0.0011519318665778979 5.4752535517383371e-06 0.061138931633108941];  % kg*mm^2
+smiData.Solid(1).color = [0.74901960784313726 0.74901960784313726 0.74901960784313726];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "encoder.stp*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.0077104108497054302;  % kg
+smiData.Solid(2).CoM = [0 0 11.899339933993399];  % mm
+smiData.Solid(2).MoI = [0.51851486270016423 0.51851486270016411 0.23633808833874331];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(2).color = [0.12549019607843137 0.082352941176470587 1];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "coupler*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.034948970638706583;  % kg
+smiData.Solid(3).CoM = [7.3982315276618422e-05 -8.390880199332484 -22.83069724554359];  % mm
+smiData.Solid(3).MoI = [17.369822997112337 17.824678012783114 24.855894250842923];  % kg*mm^2
+smiData.Solid(3).PoI = [-1.0726703289786743 -5.4639896595158696e-05 3.095399137902261e-05];  % kg*mm^2
+smiData.Solid(3).color = [0.25098039215686274 0.25098039215686274 0.25098039215686274];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "Encoder Holder*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.029761692101410914;  % kg
+smiData.Solid(4).CoM = [-0.00092449660892598247 9.1317662388571693e-05 4.5223625650556372];  % mm
+smiData.Solid(4).MoI = [25.957550590694183 25.956654742297061 49.743116715479538];  % kg*mm^2
+smiData.Solid(4).PoI = [-3.9107651893820599e-05 0.00035812206506627138 -8.8755435163857409e-05];  % kg*mm^2
+smiData.Solid(4).color = [0.25098039215686274 0.25098039215686274 0.25098039215686274];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = "Bearing Holder*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 0.005701190067439927;  % kg
+smiData.Solid(5).CoM = [36.095343075237139 32.151746246473095 0];  % mm
+smiData.Solid(5).MoI = [0.64355316896214088 6.1146477816340488 6.7194142802829901];  % kg*mm^2
+smiData.Solid(5).PoI = [0 0 -1.1005982735727875];  % kg*mm^2
+smiData.Solid(5).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = "2352959*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(6).mass = 0.15419520103839726;  % kg
+smiData.Solid(6).CoM = [0.045045335717172452 3.546066754909742 65.75396055062717];  % mm
+smiData.Solid(6).MoI = [627.68168041887304 531.84927660457311 269.80199161883212];  % kg*mm^2
+smiData.Solid(6).PoI = [-20.827997975883129 0.18851521666298346 -0.025726919644960201];  % kg*mm^2
+smiData.Solid(6).color = [0.25098039215686274 0.25098039215686274 0.25098039215686274];
+smiData.Solid(6).opacity = 1;
+smiData.Solid(6).ID = "Box*:*Default";
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(3).Rz.Pos = 0.0;
+smiData.RevoluteJoint(3).ID = "";
+
+smiData.RevoluteJoint(1).Rz.Pos = 159.87025754761191;  % deg
+smiData.RevoluteJoint(1).ID = "[encoder.stp-1:-:coupler-1]";
+
+smiData.RevoluteJoint(2).Rz.Pos = 155.38887451525346;  % deg
+smiData.RevoluteJoint(2).ID = "[coupler-1:-:2352959-1]";
+
+smiData.RevoluteJoint(3).Rz.Pos = 39.335031056051733;  % deg
+smiData.RevoluteJoint(3).ID = "[Bearing Holder-1:-:Encoder Holder-1]";
+
